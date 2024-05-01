105bbb8e50768a10bcdc54bc495111fc484303ac
Konda Yoshifumi <kndys.fm+github@gmail.com>
2024-05-01 22:47:26 +0900

    Add two other test cases for I2C

--- tests/i2c.rs
+++ tests/i2c.rs
@@ -7,6 +7,7 @@ use ft260hid::io::i2c;
 use rand::prelude::*;
 
 const EEPROM_ADDRESS: u8 = 0x50;
+const EEPROM_PAGE_SIZE: usize = 8;
 
 fn wait_in_busy(i2c: &i2c::I2c) {
   while !i2c.is_idle() { }
@@ -14,12 +15,13 @@ fn wait_in_busy(i2c: &i2c::I2c) {
 
 fn wait_write(i2c: &i2c::I2c) {
   while !i2c.is_idle() { }
+  // write cycle time 5ms typ.
   thread::sleep(Duration::from_millis(5));
 }
 
 #[test]
 fn test_i2c_basic() {
-  let mut rand = [0u8;8];
+  let mut rand = [0u8;EEPROM_PAGE_SIZE];
   thread_rng().fill(&mut rand);
 
   let dev = device::open(0).unwrap();
@@ -54,15 +56,60 @@ fn test_i2c_basic() {
   assert_eq!(rand[3], data_read[0]);
   wait_in_busy(&i2c);
 
-  // write address to read
-  let data_addr = [random::<u8>()];
+  // write address to read (clear 3 LSB bit to prevent of roll-over within a page sized in up to 8 bytes)
+  let data_addr = [random::<u8>() & 0xF8u8];
   // write 8 byte data
   assert_eq!(i2c.write(EEPROM_ADDRESS, i2c::Flag::Start, &data_addr, 1).unwrap(), 1);
   assert_eq!(i2c.write(EEPROM_ADDRESS, i2c::Flag::Stop, &rand, rand.len()).unwrap(), rand.len());
   wait_write(&i2c);
-  let mut data_read = [0u8; 8];
+  let mut data_read = [0u8; EEPROM_PAGE_SIZE];
   assert_eq!(i2c.write(EEPROM_ADDRESS, i2c::Flag::Start, &data_addr, 1).unwrap(), 1);
-  assert_eq!(i2c.read(EEPROM_ADDRESS, i2c::Flag::ReStartAndStop, &mut data_read, 8, i2c::DURATION_WAIT_DEFAULT).unwrap(), 8);
-  assert_eq!(rand[7], data_read[7]);
+  assert_eq!(i2c.read(EEPROM_ADDRESS, i2c::Flag::ReStartAndStop, &mut data_read, EEPROM_PAGE_SIZE, i2c::DURATION_WAIT_DEFAULT).unwrap(), EEPROM_PAGE_SIZE);
+  assert_eq!(rand[EEPROM_PAGE_SIZE-1], data_read[EEPROM_PAGE_SIZE-1]);
 
+}
+
+/// test repeated read write
+#[test]
+fn test_i2c_write_read() {
+  let dev = device::open(0).unwrap();
+  let mut i2c = dev.i2c();
+  assert!(i2c.init(i2c::KBPS_DEFAULT).is_ok());
+
+  for i in 0..10 {
+      let mut data = [0u8;EEPROM_PAGE_SIZE];
+      thread_rng().fill(&mut data);
+      let addr = [random::<u8>() & 0xF8u8];
+      // write 8 byte data
+      assert_eq!(i2c.write(EEPROM_ADDRESS, i2c::Flag::Start, &addr, addr.len()).unwrap(), addr.len());
+      assert_eq!(i2c.write(EEPROM_ADDRESS, i2c::Flag::Stop, &data, data.len()).unwrap(), data.len());
+      wait_write(&i2c);
+      // write-read 8 byte data
+      let mut buf = [0u8; EEPROM_PAGE_SIZE];
+      assert!(i2c.write_read(EEPROM_ADDRESS, &addr, 1, &mut buf, EEPROM_PAGE_SIZE, i2c::DURATION_WAIT_DEFAULT).is_ok());
+      assert_eq!(data[EEPROM_PAGE_SIZE-1], buf[EEPROM_PAGE_SIZE-1]);
+  }
+}
+
+/// test long read write
+#[test]
+fn test_i2c_long_data() {
+  let mut data = [0u8;256];
+  thread_rng().fill(&mut data);
+  let kbps_list = [400u16, 100u16];
+  let len_list = [256usize, 128usize, 64usize, 32usize, 16usize];
+
+  let dev = device::open(0).unwrap();
+  let mut i2c = dev.i2c();
+  for kbps in kbps_list {
+    assert!(i2c.init(kbps).is_ok());
+    wait_in_busy(&i2c);
+    for len in len_list {
+      assert_eq!(i2c.write(EEPROM_ADDRESS, i2c::Flag::StartAndStop, &data, len).unwrap(), len);
+      wait_write(&i2c);
+      let mut buf = [0u8; 256];
+      assert!(i2c.write_read(EEPROM_ADDRESS, &data, 1, &mut buf, len, i2c::DURATION_WAIT_DEFAULT).is_ok());
+      wait_in_busy(&i2c);
+    }
+  }
 }
